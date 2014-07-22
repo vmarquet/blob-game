699b8ba731c4007d92eb4b7458b3fb85f8cf3722
vmarquet <vincent.marquet1@free.fr>
2014-07-22 17:17:21 +0200

    update du LAUNCH.sh, pour ne pas executer si la compil avait fail

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 NORMAL="\\033[0;39m"  # reset
-RED="\\033[1;31m"
+BLUE="\e[1;34m"
 
+# on crée un dossier build pour ne pas mélanger les .java et les .class
 if [[ ! -d build ]]; then
 	mkdir build
 fi
 
 # compilation
 COMPIL="javac -cp . com/github/vmarquet/graph/*/*.java -d build"
-echo -e "${RED}$COMPIL${NORMAL}"
+echo -e "${BLUE}$COMPIL${NORMAL}"
 eval "$COMPIL"
 
-# execution
-EXEC="java -cp ./build com.github.vmarquet.graph.test.Test100"
-echo -e "${RED}$EXEC${NORMAL}"
-eval "$EXEC"
+# we check if compilation was successful, and if yes, we launch the program
+if [[ $? -eq 0 ]]; then
+	EXEC="java -cp ./build com.github.vmarquet.graph.test.Test100"
+	echo -e "${BLUE}$EXEC${NORMAL}"
+	eval "$EXEC"
+fi
 
 exit 0
